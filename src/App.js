72279429f1277900044d55a1f2594c4ebaf88c14
Knorton1531 <dexterandkyle@live.co.uk>
2024-04-02 07:46:38 +0100

    Disable signin

--- src/App.js
+++ src/App.js
@@ -51,7 +51,8 @@ const App = () => {
 
   return (
     <div className='pageWrapper'>
-        {!user ? <SignIn /> : <><Canvas /></>}
+      <Canvas />
+        {/* {!user ? <SignIn /> : <><Canvas /></>} */}
     </div>
   );
 };
